ab45ee1903e00d2b8d753d7a7c0e23f32623ea34
ramsudharsan08 <ramsudharsan007@gmail.com>
2025-08-02 13:23:48 +0530

    remove frontend

--- backend/app.js
+++ backend/app.js
@@ -20,13 +20,13 @@ app.use('/api/v1', product);
 app.use('/api/v1', order);
 
 // ✅ Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+//   });
+// }
 
 // ✅ Start server (for both local & Render)
 const PORT = process.env.PORT || 8000;
